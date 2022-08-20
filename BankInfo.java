4319e001eb471d6a02188987a3cf895b40d060f3
Manickand <110736710+Manickand@users.noreply.github.com>
2022-08-20 23:04:14 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,20 @@
+package week3.day1;
+
+public class BankInfo {
+	
+	public void saving()
+	{
+		System.out.println("The account is a savings account");
+	}
+	
+	public void fixed()
+	{
+		System.out.println("The fixed deposit is also available");
+	}
+	
+	public void deposit()
+	{
+		System.out.println("The deposit is not available for person 1");
+	}
+
+}
---
+++ Banking.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class Banking {
+
+	public static void main(String[] args) {
+
+		AxisBank details = new AxisBank();
+		details.deposit();
+		details.saving();
+		details.fixed();
+		BankInfo details1 = new BankInfo();
+		details1.deposit();
+
+	}
+
+}
